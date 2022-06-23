26fcdff4413d8823a0951dac3df4ff2f764410ba
Josh Daniel <joshxfi.dev@gmail.com>
2022-06-23 12:20:37 +0800

    chore: add graphql on treesitter

--- lua/treesitter-config.lua
+++ lua/treesitter-config.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup {
-  ensure_installed = {"lua", "css", "json", "vim", "typescript", "html", "tsx"},
+  ensure_installed = {"lua", "css", "json", "vim", "typescript", "html", "tsx", "graphql"},
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = true
