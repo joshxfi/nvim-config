a8ec3a91a7c3bd75e792f705f4ae1792aff92105
Josh Daniel <joshxfi.dev@gmail.com>
2022-06-24 20:26:33 +0800

    chore: add prisma to treesitter

--- lua/treesitter-config.lua
+++ lua/treesitter-config.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup {
-  ensure_installed = {"lua", "css", "json", "vim", "typescript", "html", "tsx", "graphql"},
+  ensure_installed = {"lua", "css", "json", "vim", "typescript", "html", "tsx", "graphql", "prisma"},
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = true
